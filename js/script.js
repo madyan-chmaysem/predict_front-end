2614a1f864b65f4ed0ec85911493cd82376f0fc1
madyan <chmasyemmadyan@gmail.com>
2022-09-10 20:36:29 +0300

    CSS Initial Edits

--- js/script.js
+++ js/script.js
@@ -6,3 +6,19 @@ div.appendChild(img)
 div.classList.add("imagecontainer")
 img.classList.add("center")
 img.src ="./images/download.jpg"
+const div1 = document.createElement('div');
+container.appendChild(div1)
+const p=document.createElement("p")
+div1.appendChild(p)
+p.textContent=" What name do you want to predict for?";
+const inp=document.createElement("input")
+inp.placeholder="enter the name"
+div1.appendChild(inp)
+const div2 = document.createElement('div');
+div1.appendChild(div2)
+div2.classList.add("item")
+const p1=document.createElement("p")
+div2.appendChild(p1)
+p1.textContent="Predict!"
+const div3=document.createElement("div")
+container.appendChild(div3)
