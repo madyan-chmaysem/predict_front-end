6fd2bd4c23eb0702b0c62459338c25f993b5f756
madyan <chmasyemmadyan@gmail.com>
2022-09-10 20:08:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#main-container')
+const div = document.createElement('div');
+container.appendChild(div);
+const img=document.createElement("img")
+div.appendChild(img)
+div.classList.add("imagecontainer")
+img.classList.add("center")
+img.src ="./images/download.jpg"
