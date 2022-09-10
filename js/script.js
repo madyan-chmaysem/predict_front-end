3b1a3d07f1494820263fd7d8a0e7e32501bef9c7
madyan <chmasyemmadyan@gmail.com>
2022-09-10 23:21:30 +0300

    adding the rest functional api

--- js/script.js
+++ js/script.js
@@ -26,5 +26,55 @@ div2.classList.add("item")
 const p1=document.createElement("p")
 div2.appendChild(p1)
 p1.textContent="Predict!"
-const div3=document.createElement("div")
+
+p1.addEventListener("click",()=>{
+    console.log("hi")
+    const div3=document.createElement("div")
 container.appendChild(div3)
+    const Pgendre=document.createElement("p")
+    div3.appendChild(Pgendre)
+    const div4=document.createElement("div")
+    container.appendChild(div4)
+    const Page=document.createElement("p")
+    div4.appendChild(Page)
+    const div5=document.createElement("div")
+    container.appendChild(div4)
+    const Pnatio=document.createElement("p")
+    div4.appendChild(Pnatio)
+
+    const val=inp.value
+    
+  
+     const genderAPI=" https://api.genderize.io?name="+val
+
+  
+     fetch(genderAPI)
+    .then(res=>res.json())
+    .then(data=>{ 
+        Pgendre.textContent="gender is:"+ data.gender
+    })
+      
+       const Page1=" https://api.agify.io/?name="+val
+      
+     fetch(Page1)
+    .then(res=>res.json())
+    .then(data=>{ 
+       
+      
+       Page.textContent="gender is:"+ data.age
+
+
+})
+const Pnatio1=" https://api.nationalize.io/?name="+val
+
+fetch(Pnatio1)
+.then(res=>res.json())
+.then(data=>{ 
+
+    Pnatio.textContent="nationality is : "+data.country[0]["country_id"]+" / "+data.country[1]["country_id"]
+
+
+})
+
+
+})
